5eb19a709c20be30d5ac9feafe7a0439a546b0fb
Sudipto Roy <sudipto.roy@brainstation-23.com>
2023-05-30 09:36:29 +0600

    Initial Commit

---
+++ CustomTabbar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomTabbar
+//
+//  Created by Sudipto Roy on 30/5/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
